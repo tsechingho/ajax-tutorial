39d450dbf0ea811a0b94004c991d70b62b6a6900
Tse-Ching Ho <tsechingho@gmail.com>
2012-08-20 15:52:19 +0800

    rails new ajax-tutorial -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxTutorial::Application.config.secret_token = 'ded968fb6fd4a49f80cb130cdf82c74be536b0e1354d776de5d97a4bd21172f73bc2acfdcfc8c4603f1ffe730e9787f8e21cbbcdddcdea5bdaaa1318ecc7cc23'
