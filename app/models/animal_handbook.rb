07e795f04a7b5f1848f5a0b75538952ffa0560ad
Tse-Ching Ho <tsechingho@gmail.com>
2012-08-21 00:34:23 +0800

    apply cocoon to animal handbook

--- app/models/animal_handbook.rb
+++ app/models/animal_handbook.rb
@@ -1,8 +1,9 @@
 class AnimalHandbook < ActiveRecord::Base
-  attr_accessible :name, :creature_ids
+  attr_accessible :name, :creature_ids, :creatures_attributes
 
   validates :name, presence: true
 
   has_many :animal_handbook_creatures
   has_many :creatures, through: :animal_handbook_creatures
+  accepts_nested_attributes_for :creatures, allow_destroy: true, reject_if: proc { |obj| obj.blank? }
 end
