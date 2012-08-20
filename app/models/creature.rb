b1c07996273eac17a5be87713bfb85d7faf9f2c4
Tse-Ching Ho <tsechingho@gmail.com>
2012-08-21 01:52:22 +0800

    apply cocoon to creature

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,11 +1,12 @@
 class Creature < ActiveRecord::Base
-  attr_accessible :characteristic, :place_of_origin, :popular_name, :scientific_name, :animal_handbook_ids
+  attr_accessible :characteristic, :place_of_origin, :popular_name, :scientific_name, :animal_handbook_ids, :photos_attributes
 
   validates :popular_name, presence: true
 
   has_many :animal_handbook_creatures
   has_many :animal_handbooks, through: :animal_handbook_creatures
   has_many :photos, class_name: 'CreaturePhoto'
+  accepts_nested_attributes_for :photos, allow_destroy: true, reject_if: proc { |obj| obj.blank? }
 
   def name
     popular_name
