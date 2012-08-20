6cc220c45d60a2595a4eb28f616f032febc45239
Tse-Ching Ho <tsechingho@gmail.com>
2012-08-20 20:13:38 +0800

    let's upload creature photos

--- app/models/creature_photo.rb
+++ app/models/creature_photo.rb
@@ -1,5 +1,28 @@
+require 'file_size_validator'
+
 class CreaturePhoto < ActiveRecord::Base
-  attr_accessible :content_type, :file_name, :file_size, :creature_id
+  attr_accessible :content_type, :file_name, :file_size, :creature_id, :source, :source_cache, :remove_source
+
+  validates :source,
+    file_size: {
+      maximum: 3.megabytes.to_i
+    }
+
+  mount_uploader :source, CreaturePhotoUploader, mount_on: :file_name
+  delegate :url, :current_path, :size, :filename, to: :source
 
   belongs_to :creature
+
+  before_save :update_attributes_with_source
+
+  protected
+
+  def update_attributes_with_source
+    if file_name.present? && file_name_changed?
+      self.content_type = source.file.content_type
+      self.file_size = source.file.size
+    elsif file_name.blank?
+      self.content_type = self.file_size = nil
+    end
+  end
 end
--- app/uploaders/creature_photo_uploader.rb
+++ app/uploaders/creature_photo_uploader.rb
@@ -1,10 +1,12 @@
 # encoding: utf-8
+require 'carrierwave/processing/mime_types'
 
 class CreaturePhotoUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MimeTypes
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
   # include Sprockets::Helpers::RailsHelper
@@ -13,6 +15,8 @@ class CreaturePhotoUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+
+  process :set_content_type
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -36,15 +40,16 @@ class CreaturePhotoUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    # process scale: [50, 50]
+    process :resize_to_fit => [50,50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
